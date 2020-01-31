bcddb7b9445c41f897e902b97cff0c502f7d7fe8
nul00000000 <nulcreatesgames@gmail.com>
2020-01-31 12:44:02 -0500

    EmErGeNcY cOmMiT

---
+++ src/io/github/nul00000000/Main.java
@@ -0,0 +1,15 @@
+package io.github.nul00000000;
+
+public class Main {
+	
+	public static final byte INFINTE_RECHARGE = 0x0;
+	public static final byte TURTLE_SCREAM = 0x1;
+	
+	public static void main(String[] args) {
+		System.out.println("Program started");
+		if(args.length < 1) {
+			System.out.println("No program selected.");
+		}
+	}
+
+}
